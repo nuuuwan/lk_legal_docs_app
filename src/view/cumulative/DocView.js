386359f2ee13c40630c623eaef4c7a274718b31e
nuuuwan <nuuuwan@gmail.com>
2025-07-10 14:08:36 +0530

    Removed styling

--- src/view/cumulative/DocView.js
+++ src/view/cumulative/DocView.js
@@ -2,7 +2,7 @@ import { Box, Typography } from "@mui/material";
 
 export default function DocView({ doc }) {
   return (
-    <Box sx={{ bgcolor: "#eee", p: 1, borderRadius: 1, width: "100%" }}>
+    <Box>
       <Typography variant="subtitle2">{doc.date}</Typography>
       <Typography variant="subtitle1">
         {doc.docType.emoji + " " + doc.docType.name}
