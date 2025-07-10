7f4549ffa9dccf1c7ebb08c3759bf0060f1ebe73
nuuuwan <nuuuwan@gmail.com>
2025-07-10 12:22:47 +0530

    Use DocType

--- src/nonview/core/DocMetadata.js
+++ src/nonview/core/DocMetadata.js
@@ -1,31 +1,28 @@
 import { WWW } from "../base";
+import DocType from "../core/DocType.js";
 export default class DocMetadata {
   static URL_DOCS_ALL =
     "https://raw.githubusercontent.com" +
     "/nuuuwan/lk_legal_docs_data/refs/heads/main" +
     "/latest-100.json";
 
-  constructor(doc_type_name, id, date, description) {
-    this.doc_type_name = doc_type_name;
+  constructor(docTypeName, id, date, description) {
+    this.docTypeName = docTypeName;
     this.id = id;
     this.date = date;
     this.description = description;
   }
-  toDict() {
-    return {
-      doc_type_name: this.doc_type_name,
-      id: this.id,
-      date: this.date,
-      description: this.description,
-    };
+
+  get docType() {
+    return DocType.fromDocTypeName(this.docTypeName);
   }
 
-  static fromDict(dict) {
+  static fromDict(d) {
     return new DocMetadata(
-      dict.doc_type_name,
-      dict.id,
-      dict.date,
-      dict.description
+      d["doc_type_name"],
+      d["id"],
+      d["date"],
+      d["description"],
     );
   }
 
