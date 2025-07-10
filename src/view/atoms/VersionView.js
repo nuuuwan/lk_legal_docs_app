e0cd12486de94b1bb95f95603afb3b494cc41e13
nuuuwan <nuuuwan@gmail.com>
2025-07-10 17:46:53 +0530

    Added Refresh to VersionView

--- src/view/atoms/VersionView.js
+++ src/view/atoms/VersionView.js
@@ -1,8 +1,18 @@
-import { Box, Typography } from "@mui/material";
+import { Box, IconButton, Typography } from "@mui/material";
 import { VERSION } from "../../nonview/constants";
+import RefreshIcon from "@mui/icons-material/Refresh";
 export default function VersionView() {
+  const onClick = function () {
+    localStorage.clear();
+    window.location.reload();
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
   return (
-    <Box sx={{ textAlign: "center", m: 1 }}>
+    <Box sx={{ textAlign: "center", m: 2, mb: 20 }}>
+      <IconButton onClick={onClick}>
+        <RefreshIcon />
+      </IconButton>
       <Typography variant="caption" color="#888">
         App Version-{VERSION.DATETIME_STR}
       </Typography>
