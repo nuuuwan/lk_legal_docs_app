9d6ffdf4e1467c1c27a57ece2cd0d5022098e3a0
nuuuwan <nuuuwan@gmail.com>
2025-07-10 14:06:32 +0530

    ListItemButton

--- src/view/cumulative/DocListView.js
+++ src/view/cumulative/DocListView.js
@@ -1,12 +1,12 @@
-import { List, ListItem } from "@mui/material";
+import { List, ListItemButton } from "@mui/material";
 import DocView from "./DocView";
 export default function DocListView({ docList }) {
   return (
     <List>
       {docList.map((doc) => (
-        <ListItem key={doc.id}>
+        <ListItemButton key={doc.id}>
           <DocView doc={doc} />
-        </ListItem>
+        </ListItemButton>
       ))}
     </List>
   );
