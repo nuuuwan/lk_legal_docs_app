968323ca5f009c252c9e9d6c0642ae059aea3910
nuuuwan <nuuuwan@gmail.com>
2025-07-11 18:20:56 +0530

    Removed Title Pane

--- src/view/molecules/SearchBox.js
+++ src/view/molecules/SearchBox.js
@@ -1,16 +1,8 @@
-import { Box, TextField, Typography } from "@mui/material";
+import { Box, TextField } from "@mui/material";
 import SearchBoxInfo from "../atoms/SearchBoxInfo";
-import { STYLE } from "../../nonview/constants";
 export default function SearchBox({ searchKey, docList, onChange }) {
   return (
     <Box sx={{ m: 1, p: 1 }}>
-      <Typography
-        variant="body1"
-        sx={{ mb: 1, textAlign: "center" }}
-        color={STYLE.COLOR.LIGHTER}
-      >
-        🇱🇰 Sri Lanka - Legal Document Search
-      </Typography>
       <TextField
         variant="outlined"
         placeholder="Search..."
